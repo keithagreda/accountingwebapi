d4ae7475f8177b43b3f6a31be862888b8e758bb5
kkitoo21 <104190595+kkitoo21@users.noreply.github.com>
2025-05-08 22:47:57 +0800

    Update ModelBuilderExtensions.cs

--- accountingwebapi/Extensions/ModelBuilderExtensions.cs
+++ accountingwebapi/Extensions/ModelBuilderExtensions.cs
@@ -1,6 +1,28 @@
-﻿namespace accountingwebapi.Extensions
+﻿using Microsoft.EntityFrameworkCore;
+using NUlid;
+
+namespace accountingwebapi.Extensions
 {
-    public class ModelBuilderExtensions
+    public static class ModelBuilderExtensions
     {
+        public static void ApplyUlidConversion(this ModelBuilder modelBuilder)
+        {
+            foreach (var entityType in modelBuilder.Model.GetEntityTypes())
+            {
+                var properties = entityType.GetProperties()
+                    .Where(p => p.PropertyInfo.PropertyType == typeof(Ulid));
+
+                foreach (var property in properties)
+                {
+                    modelBuilder.Entity(entityType.ClrType)  // Use entityType.ClrType instead of entityType.Name
+                        .Property(property.Name)
+                        .HasConversion(
+                            id => id.ToString(),  // Convert Ulid to string for storage
+                            str => Ulid.Parse(str)  // Convert string back to Ulid when reading from DB
+                        );
+                }
+            }
+        }
     }
+
 }
